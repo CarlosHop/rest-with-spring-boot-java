014bf8ac7de866cc9e3c90a4441ac42ead38fc23
Carlos Henrique <84361593+CarlosHop@users.noreply.github.com>
2023-02-02 16:03:26 -0300

    Basic archivies for API

---
+++ rest-with-spring-boot-java-carlos/src/main/java/br/example/rest/RestWithSpringBootJavaCarlosApplication.java
@@ -0,0 +1,13 @@
+package br.example.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootJavaCarlosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWithSpringBootJavaCarlosApplication.class, args);
+    }
+
+}
---
+++ rest-with-spring-boot-java-carlos/src/test/java/br/example/rest/RestWithSpringBootJavaCarlosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.example.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootJavaCarlosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
